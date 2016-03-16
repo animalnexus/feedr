5855c1182c9b4b576e6ab175fa6fd6dad6d3a539
steffilazerte <steffi@steffi.ca>
2016-03-16 16:09:58 -0700

    New function get.data()
    Create new function to download data directly from birdmoves website. Modify load.web() to accept data frame for formatting.

--- R/load.R
+++ R/load.R
@@ -1,17 +1,18 @@
-#' Load read data from the web
+
+#' Load read data from the web (legacy)
 #'
-#' Loads raw read data from csv files downloaded from the \url{http://gaia.tru.ca/birdMOVES/datadownload.html} website and formats them for use with the feedr functions. This is merely a wrapper function that does many things that you can do yourself. It's utility depends on how standardized your data is, and whether you have extra details you need to address.
+#' Loads raw read data from csv files downloaded from the __old__ version of \url{http://gaia.tru.ca/birdMOVES/datadownload.html} website and formats them for use with the feedr functions. This is merely a wrapper function that does many things that you can do yourself. It's utility depends on how standardized your data is, and whether you have extra details you need to address.
 #'
 #' @param r_file Character. The location of a single file to load.
 #' @param tz Character. The time zone the date/times should be converted to (should match one of the zones produced by \code{OlsonNames())}.
 #' @param sep Character. An override for the separator in the \code{read.table()} call (see \code{sep =} under \code{?read.table} for more details).
 #'
 #' @examples
-#' \dontrun{r <- load.web("downloaded_file.csv")}
+#' \dontrun{r <- load.web.legacy("downloaded_file.csv")}
 
 
 #' @export
-load.web <- function(r_file, tz = "Canada/Pacific", sep = ",") {
+load.web.legacy <- function(r_file, tz = "Canada/Pacific", sep = ",") {
   r <- read.table(r_file, sep = ",", col.names = c("date","feeder_id","bird_id"))
   r$time <- as.POSIXct(strptime(r$date, "%m-%d-%yT%H:%M:%SZ", tz = "Zulu"))
   attributes(r$time)$tzone <- tz
@@ -19,26 +20,25 @@ load.web <- function(r_file, tz = "Canada/Pacific", sep = ",") {
   return(r)
 }
 
-#' Load read data from the web (update)
+#' Load read data from the web
 #'
-#' Loads raw read data from csv files downloaded from the \url{http://gaia.tru.ca/birdMOVES/datadownload.html} website and formats them for use with the feedr functions. This is merely a wrapper function that does many things that you can do yourself. It's utility depends on how standardized your data is, and whether you have extra details you need to address. This is updated to work with newer data download formats
+#' Loads raw read data from csv files downloaded from the __current__ \url{http://gaia.tru.ca/birdMOVES/datadownload.html} website and formats them for use with the feedr functions. This is merely a wrapper function that does many things that you can do yourself. It's utility depends on how standardized your data is, and whether you have extra details you need to address. This is updated to work with newer data download formats.
 #'
-#' @param r_file Character. The location of a single file to load.
-#' @param tz.to Character. The time zone the date/times should be converted to (should match one of the zones produced by \code{OlsonNames())}.
-#' @param tz.from Character. The time zone the date/times should be converted from (should match one of the zones produced by \code{OlsonNames())}.
+#' This function is also used by \code{\link{get.data}} and so can accept a data frame in place of a file location.
+#'
+#' @param r_file Character or Data frame. The location of a single file to load, or a data frame of raw reads to be formated.
+#' @param tz Character. The time zone the date/times are in (should match one of the zones produced by \code{OlsonNames())}.
 #' @param sep Character. An override for the separator in the \code{read.table()} call (see \code{sep =} under \code{?read.table} for more details).
 #'
 #' @examples
 #' \dontrun{r <- load.web("downloaded_file.csv")}
 
 #' @export
-load.web2 <- function(r_file, tz.from = "GMT", tz.to = "Canada/Pacific", sep = ",") {
-  r <- read.csv(r_file)
-  r <- r[, c("feeder_id","bird_id","timezone")]
-  names(r)[3] <- "time"
-  r$time <- as.POSIXct(r$time, tz = tz.from)
-  attributes(r$time)$tzone <- tz.to
-  r <- r[,c("bird_id","time", "feeder_id")]
+load.web <- function(r_file, tz = "Canada/Pacific", sep = ",") {
+  if(!is.data.frame(r_file)) r <- r[, c("feeder_id","bird_id","timezone")] else r <- r_file
+  names(r)[names(r) == "timezone"] <- "time"
+  r$time <- as.POSIXct(r$time, tz = tz)
+  r <- r[,c("bird_id","time", "feeder_id", names(r)[!(names(r) %in% c("bird_id", "time", "feeder_id"))])]
   return(r)
 }
 
@@ -100,3 +100,97 @@ load.raw <- function(r_file, tz = "Canada/Pacific", feeder_pattern = "[GPR]{2,3}
     } else if(!is.null(extra_name)) stop("You have specified names for extra columns, but you have not specified what pattern to match for filling ('extra_pattern').")
     return(r)
 }
+
+
+#' Download data from Thompson Rivers University "BirdMoves" website.
+#'
+#' This function uses RCurl to submit an HTML form and retrieve the csv file. This is simply a convenience function to replace going to the website yourself (http://gaia.tru.ca/birdMOVES/datadownload.html) and then running the data frame through \code{\link{load.web}}.
+#'
+#' Note that while the website requires a date in the format of YYYY-MM-DD HH:MM:SS, this function is a bit more flexible. Using \code{\link[lubridate]{parse_date_time}} from the lubridate package, the format of the date/time supplied will estimated. This allows for partial date/times to be supplied (e.g., "2015-01-01 09" or "2015-09" or "2015-09-01"). For best results, specify the date/time format as YYYY-MM-DD HH:MM:SS and truncate as needed. Note that truncated times are interpreted as 00 and trucated dates as the first of the month and the first month of the year. Thus "2015" will be sumbitted as "2015-01-01 00:00:00".
+#'
+#' For specifying extra \code{bird_details}:
+#'
+#' - "species" gives species name
+#' - "sex" gives sex (M, F, U for male, female and unknown)
+#' - "age" gives age (ASY, AHY, SY, and HY for After Second Year, After Hatch Year, Second Year, and Hatch Year)
+#'   See \href{https://www.pwrc.usgs.gov/BBL/manual/age.cfm}{USGS banding lab} for details about banding
+#' - "tagged_on" gives you the date the RFID tag was mounted on the bird
+#'
+#' For specifying extra \code{feeder_details}:
+#'
+#' - "site_name" gives site name
+#' - "loc" gives lon/lat coordiates
+#'
+#' @param start Character. This is the start date (with or without time) for the data to download. There is some flexibility in the format (see details). If NULL, get records from start.
+#' @param end  Character. This is the end date (with or without time) for the data to download. There is some flexibility in the format (see details). If NULL, get records to end.
+#' @param url Character. This is the url for the web form action. _Not_ the url where users go to download their data by hand. The default should not need to be changed.
+#' @param feeder_details Character vector. This specifies extra columns with details about the feeders to download. Use NULL to download no extra columns.
+#' @param bird_details Character vector. This specifies extra columns with details about the birds to download. Use NULL to download no extra columns.
+#' @param tz Character vector. Timezone to use for specifying and downloading data. Timezone must be one of "America/Vancouver" (default), "America/Costa Rica", or "GMC".
+#' @param sites Character vector. Sites to download data from. Currently must be one or more of "Kamloops" (default) or "Costa Rica".
+#'
+#' @examples
+#' \dontrun{
+#' # Get all data (may take a couple minutes)
+#' r <- get.data()
+#'
+#' # Get all 2016 data
+#' r <- get.date(start = "2016")
+#'
+#' # Get specific data
+#' r <- get.data(start = "2016-01-01 09:34:12", end = "2016-02-01", bird_details = c("species", "age", "sex", "tagged_on"))
+#' }
+#'
+#' @export
+get.data <- function(start = NULL,
+                     end = NULL,
+                     url = "http://gaia.tru.ca/birdMOVES/rscripts/rawvisits.csv",
+                     feeder_details = c("loc"),
+                     bird_details = c("species"),
+                     tz = "America/Vancouver",
+                     sites = "Kamloops") {
+
+  # Stop if timezones different
+  if(!(tz %in% c("America/Vancouver", "GMT", "America/Costa Rica"))) stop("Timezone must be one of 'America/Vancouver', 'GMT', 'America/Costa Rica'. (You can change timezones after they've been downloaded.)")
+
+  # Stop if time is not in the correct format
+
+  t.start <- NULL
+  t.end <- NULL
+  if(!is.null(start)) {
+    suppressWarnings(t.start <- lubridate::parse_date_time(start, orders = "ymd hms", truncated = 5))
+    if(is.na(t.start)) stop("Your start time is ambiguous. Format should be YYYY-MM-DD (HH:MM:SS is optional)")
+    t.start <- format(t.start, "%Y-%m-%d %H:%M:%S")
+  }
+  if(!is.null(end)) {
+    suppressWarnings(t.end <- lubridate::parse_date_time(end, orders = "ymd hms", truncated = 5))
+    if(is.na(t.end)) stop("Your end time is ambiguous. Format should be YYYY-MM-DD (HH:MM:SS is optional)")
+    t.end <- format(t.end, "%Y-%m-%d %H:%M:%S")
+  }
+
+  # Stop if url doesn't exist
+  if(!url.exists(url)) stop("The url '", url, "' doesn't exist (or you have no internet connection).")
+
+  # Stop if sites not in list
+  if(!all(sites %in% c("Kamloops", "Costa Rica"))) stop("Sites must be one or more of 'Kamloops' and/or 'Costa Rica'")
+
+  sites <- gsub("Kamloops", "qskam", sites)
+  sites <- gsub("Costa Rica", "qscr", sites)
+
+  # Get form options
+  params <- as.list(rep("1", length(c(feeder_details, bird_details, sites))))
+  names(params) <- c(feeder_details, bird_details, sites)
+  params <- append(params,
+                   list(feeder_id = "1",
+                        bird_id = "1",
+                        tz = tz,
+                        qstart = t.start,
+                        qend = t.end,
+                        qstarttz = tz,
+                        qendtz = tz))
+
+  g <- RCurl::getForm(url, .params = params)
+  r <- load.web(read.csv(text = g), tz = tz)
+  return(r)
+}
+
