b8cb8c25e3a461c0b3e3cc96a929dd99d361bce5
steffilazerte <steffi@steffi.ca>
2016-03-23 16:38:35 -0700

    Update function name and corresponding documentation

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@ col.order <- function(d, n){
 }
 
 
-#' Internal Movement path function (unique for each feeder path)
+# Internal Movement path function (unique for each feeder path)
 mp <- function(x) {
   x$move_path <- paste(as.character(sort(unlist(x[,c("feeder_left","feeder_arrived")]))),collapse = "_")
   return(x)
