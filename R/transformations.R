116e24b3407b5731052218d0b87b97c6bd010575
steffilazerte <steffi@steffi.ca>
2016-04-11 14:15:13 -0700

    Update usage of keep.extra() to allow only one of bird_id or feeder_id.

--- R/transformations.R
+++ R/transformations.R
@@ -1,5 +1,5 @@
 # Grab extra columns unique only
-keep.extra <- function(d, n){
+keep.extra <- function(d, n, only = c("bird_id", "feeder_id")){
 
   d <- unique(d[, setdiff(names(d), n)])
 
@@ -10,13 +10,15 @@ keep.extra <- function(d, n){
     d <- d[, names(d) != "loc",]
   }
 
-  if(nrow(d) > 1) {
-    bird_id <- names(which(lapply(d[, !(names(d) %in% c("bird_id", "feeder_id"))], FUN = function(x) nrow(unique(cbind(d$bird_id, x)))) == length(unique(d$bird_id))))
-    feeder_id <- names(which(lapply(d[, !(names(d) %in% c("bird_id", "feeder_id"))], FUN = function(x) nrow(unique(cbind(d$feeder_id, x)))) == length(unique(d$feeder_id))))
+  if("bird_id" %in% only) bird_id <- names(which(lapply(d[, !(names(d) %in% c("bird_id", "feeder_id"))], FUN = function(x) nrow(unique(cbind(d$bird_id, x)))) == length(unique(d$bird_id)))) else bird_id <- NULL
+  if("feeder_id" %in% only) feeder_id <- names(which(lapply(d[, !(names(d) %in% c("bird_id", "feeder_id"))], FUN = function(x) nrow(unique(cbind(d$feeder_id, x)))) == length(unique(d$feeder_id)))) else feeder_id <- NULL
 
+  if(all(c("feeder_id", "bird_id") %in% only)) {
     both <- intersect(bird_id, feeder_id)
     feeder_id <- setdiff(setdiff(feeder_id, both), bird_id)
     bird_id <- setdiff(setdiff(bird_id, both), feeder_id)
+  } else {
+    both <- NULL
   }
 
   if(length(both) > 0) both <- unique(d[, c("bird_id", "feeder_id", both)]) else both <- NULL
@@ -25,7 +27,7 @@ keep.extra <- function(d, n){
   return(list(both = both, bird_id = bird_id, feeder_id = feeder_id))
 }
 
-merge.extra <- function(d, extra) {
+merge.extra <- function(d, extra, only = NULL) {
   if(!is.null(extra$both)) d <- merge(d, extra$both, by = c("bird_id", "feeder_id"), all.x = TRUE, all.y = FALSE)
   if(!is.null(extra$bird_id)) d <- merge(d, extra$bird_id, by = "bird_id", all.x = TRUE, all.y = FALSE)
   if(!is.null(extra$feeder_id)) d <- merge(d, extra$feeder_id, by = "feeder_id", all.x = TRUE, all.y = FALSE)
