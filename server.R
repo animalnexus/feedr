8e1eb5305356b877bd2f1a89077bd8f089aa63a3
steffilazerte <steffi@steffi.ca>
2016-05-05 17:43:18 -0700

    Get bird data and split

--- server.R
+++ server.R
@@ -41,16 +41,19 @@ shinyServer(function(input, output) {
   
   #Sys.setenv(TZ = "")
   con <- dbConnect(drv,host=dbhost,port=dbport,dbname=dbname,user=dbuser,password=dbpass)
-  
+
   #feeders <- dbGetQuery(con, statement = paste0("SELECT * FROM feeders")) %>%
   #  mutate(loc = gsub("\\(|\\)", "", loc)) %>%
   #  separate(loc, into = c("lon", "lat"), ",", convert = TRUE)
+  
+  birds <- dbGetQuery(con, statement = paste0("SELECT * FROM birds"))
   
   suppressWarnings(
   data <- dbGetQuery(con, statement = paste("SELECT bird_id, feeder_id, time",
                                             "FROM raw.visits",
                                             "WHERE time > CURRENT_TIMESTAMP - INTERVAL '2 month'")) %>%
-    left_join(dbGetQuery(con, statement = paste0("SELECT * FROM feeders")), by = "feeder_id") %>%
+    left_join(dbGetQuery(con, statement = "SELECT * FROM feeders"), by = "feeder_id") %>%
+    left_join(dbGetQuery(con, statement = "SELECT * FROM birds"), by = c("bird_id", "site_name")) %>%
     load.format(., tz = "")
   )
   
@@ -60,6 +63,11 @@ shinyServer(function(input, output) {
     select(feeder_id, site_name, lon, lat) %>%
     unique(.)
 
+  birds <- data %>%
+    select(bird_id, species, age, sex, tagged_on, site_name) %>%
+    unique(.)
+  
+  # Data for animated maps
   v <- visits(data) %>% 
     mutate(day = as.Date(start)) %>%
     group_by(day, feeder_id, lat, lon)
@@ -78,8 +86,20 @@ shinyServer(function(input, output) {
   
   v <- v %>% group_by(feeder_id, lat, lon)
   
-  # f <- v %>% group_by(bird_id) %>% do(feeding(.))
-  # m <- v %>% group_by(bird_id) %>% do(move(.)) %>%
+  # Data for static maps
+  m <- v %>% 
+    group_by(bird_id) %>% 
+    do(move(.)) %>%
+    group_by(bird_id, feeder_id, move_path) %>%
+    summarize(path_use = length(move_path)) %>%
+    arrange(bird_id, move_path)
+                
+  f <- v %>% 
+    group_by(bird_id) %>% 
+    do(feeding(.)) %>%
+    group_by(bird_id, feeder_id) %>%
+    summarize(feed_length = sum(feed_length))
+
   #   filter(bird_id == "0620000514")
   # 
   # f %>% filter(feed_start >= v$start[1],
@@ -90,140 +110,36 @@ shinyServer(function(input, output) {
   #   summarize(a = sum(activity))
   
 
-  #output$data <- DT::renderDataTable(
-  #  if(!is.null(data)) DT::datatable(data, options = list(pageLength = 10))
-  #)
   
- # output$data <- DT::renderDataTable(
-#    DT::datatable(data, rownames = FALSE)
-#  )
- # output$v <- DT::renderDataTable(
-  #  DT::datatable(v, rownames = FALSE)
-  #)
-  
-  ### Interactive UI
-  output$time_UI <- renderUI({
-    req(v, input$speed, input$interval)
-    sliderInput("time", "Time",
-                min = floor_date(min(v$start), unit = "day"), 
-                max = ceiling_date(max(v$start), unit = "day"),
-                value = floor_date(min(v$start), unit = "day"),
-                step = 60*60*input$interval,
-                animate = animationOptions(interval = 1000 * (1 - input$speed/100), loop = TRUE),
-                #animate = animationOptions(interval = 500, loop = TRUE),
-                width = "100%")
-  })
-  
-  ### Visualizations (look at points on leaflet map)
-  # Get a point
-  # Get a slider bar for looking at time and change over time
-  
-  v_info <- reactive({
-    req(v, t_visits, b_visits, t_birds, input$birds)
-    temp <- NULL
-    if(input$birds == "visits") temp <- v %>% mutate(n = 100)
-    if(input$birds == "t_visits") temp <- t_visits
-    if(input$birds == "b_visits") temp <- b_visits
-    if(input$birds == "t_birds") temp <- t_birds
-    temp
-  })
-  
-  v_points <- reactive({
-    req(v, input$time, input$interval, input$birds)
+  ## Load output UIs
+  source("output_UI.R", local = TRUE)
+                
+  ## Load output data tables
+  source("output_data.R", local = TRUE)
     
-    temp <- v %>% filter(start >= input$time[1], start < input$time[1] + 60 * 60 * input$interval)
-    if(input$birds == "t_visits") temp <- temp %>% summarize(n = length(start))
-    if(input$birds == "b_visits") {
-      temp <- temp %>% 
-        group_by(bird_id, add = TRUE) %>% 
-        summarize(n = length(start)) %>%
-        group_by(feeder_id, lat, lon) %>%
-        summarize(n = mean(n))
-    }
-    if(input$birds == "t_birds") temp <- temp %>% summarize(n = length(unique(bird_id)))
-    temp
+  ## Load reactive expressions
+  source("reactive.R", local = TRUE)
+  
+  ### Visualizations
+  source("map_animated.R", local = TRUE)
+  source("map_static.R", local = TRUE)
+  
+  ## Look at birds
+  output$img_birds <- renderText({
     
-  })
-
-   output$points <- DT::renderDataTable(
-      DT::datatable(v_points(), rownames = FALSE)
-    )
-   
-   output$selection <- renderText({input$birds})
-  
-  # a_points <- reactive({
-  #   req(f, input$time, input$interval)
-  #   browser()
-  #   f %>% filter(feed_start >= input$time[1],
-  #                feed_start < input$time[1] + 60*60*interval) %>%
-  #     group_by(bird_id) %>%
-  #     do(activity(., res = 1, by_feeder = TRUE)) %>%
-  #     filter(time < input$time[1] + 60*60*interval) %>%
-  #     summarize(a = sum(activity))
-  # })
-  
-  output$map <- renderLeaflet({
-    pal <- colorNumeric(palette = colorRampPalette(c("blue", "green", "yellow","orange", "red"))(max(v_info()$n)), domain = 1:max(v_info()$n))
-    #pal <- colorNumeric(palette = heat.colors(max(v_info()$n)), domain = 1:max(v_info()$n))
-    #pal <- colorNumeric(palette = colorRampPalette(c("yellow","red"))(10), domain = 1:100)
-    
-    suppressMessages(
-      m <- leaflet(feeders) %>%
-        addMarkers(group = "Feeders") %>%
-        addTiles(group = "Default (Open Street Map)") %>%
-        addProviderTiles("Stamen.Toner", group = "Black and White") %>%
-        addProviderTiles("Esri.WorldTopoMap", group = "Terrain") %>%
-        addProviderTiles("Esri.WorldImagery", group = "Satelite") %>%
-        addLayersControl(overlayGroups = c("Feeders", "Visits"),
-                         baseGroups = c("Default (Open Street Map)", 
-                                        "Black and White", 
-                                        "Satelite",
-                                        "Terrain"),
-                          options = layersControlOptions(collapsed = FALSE))
-      )
-    if(input$birds != "visits") {
-      m <- m %>%
-        addLegend(title = "Legend",
-                  position = 'topright',
-                  pal = pal,
-                  values = 1:max(v_info()$n),
-                  bins = 5,
-                  opacity = 1)
-    }
-    m
-  })
-  
-  observe({
-    pal <- colorNumeric(palette = colorRampPalette(c("blue","green", "yellow","orange", "red"))(max(v_info()$n)), domain = 1:max(v_info()$n))
-    ## Change markers depending on time
-    if(nrow(v_points()) > 0){
-      
-      suppressMessages(
-        leafletProxy("map") %>%
-          clearGroup(group = "Visits")
-        )
-      
-      if(input$birds == "visits") {
-        suppressMessages(
-          leafletProxy("map") %>%
-          addCircleMarkers(data = v_points(), group = "Visits", stroke = FALSE, fillOpacity = 1)
-        )
-      }
-
-       if(input$birds != "visits") {
-         suppressMessages(
-           leafletProxy("map") %>%
-             addCircleMarkers(data = v_points(), group = "Visits",
-                              stroke = FALSE,
-                              fillOpacity = 1,
-                              radius = ~scale_area(n, val.max = max(v_info()$n)),
-                              fillColor = ~pal(n),
-                              popup = ~htmltools::htmlEscape(as.character(round(n, 1))))
-         )
-       }
+    if(is.null(input$birds_rows_selected) | is.null(birds)) {
+      temp <-" https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Bird01.svg/442px-Bird01.svg.png"
     } else {
-      leafletProxy("map") %>% clearGroup(group = "Visits")
+      r <- input$birds_rows_selected
+      
+      if(birds$species[r] == "House Finch") {
+      temp <- "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/House_Finch-27527-2.jpg/320px-House_Finch-27527-2.jpg"
+      } else if(birds$species[r] == "Mountain Chickadee") {
+      temp <- "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Poecile_gambeli%2C_Walden%2C_Colorado_1.jpg/320px-Poecile_gambeli%2C_Walden%2C_Colorado_1.jpg"
+      }
     }
-  })
+    paste("<img src='",temp,"' height = 300>")
+    })
+
   
 })
