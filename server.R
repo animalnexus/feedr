29234a7bf31e9fb11c715a50d280f1603eeb97e2
steffilazerte <steffi@steffi.ca>
2016-05-31 09:41:26 -0700

    Big update - iNat integration - homerange estimates - static UI updates with button

--- server.R
+++ server.R
@@ -4,7 +4,7 @@
 
 ## devtools::install_github("steffilazerte/feedr")
 
-## OR: 
+## OR:
 #library(rsconnect)
 
 # This is the server logic for a Shiny web application.
@@ -13,17 +13,32 @@
 # http://shiny.rstudio.com
 #
 
+## Framework
 library(shiny)
 library(shinyjs)
-library(RPostgreSQL)
+library(htmltools)
+
+## Visualizations
+library(leaflet)
+library(ggplot2)
+##library(ggvis)
+
+## Data manip
 library(tidyr)
 library(dplyr)
 library(feedr)
-library(leaflet)
-library(ggplot2)
 library(lubridate)
-library(htmltools)
-#library(ggvis)
+
+## Data aquisition
+library(rinat)
+library(RPostgreSQL)
+
+## Homerange
+library(sp)
+library(KernSmooth)
+
+## Functions
+source("functions.R", local = TRUE)
 
 dbname <- 'birdmv-dev'
 dbuser <- 'tern'
@@ -34,103 +49,113 @@ dbhost <- 'geoteach.tru.ca'
 
 imgs <- read.csv("./data/species.csv", colClasses = c("factor", "character"))
 
-scale_area <- function(r, val.max = max(r, na.rm = TRUE), min = 5, max = 105){
-  if(val.max == 1) val.max <- 5
-  r <- ((r-1) / (val.max - 1)) * (max - min)
-  r[r >= 0] <- r[r >= 0] + min
-  r[r < 0] <- 0
-  return(r)
-}
-
 qry <- function(x) paste0(x, collapse = "', '")
 
+#withProgress(message = "Connecting to server...", {
+suppressWarnings({
+  cat("Connecting to server...\n")
+  con <- dbConnect(drv,host=dbhost,port=dbport,dbname=dbname,user=dbuser,password=dbpass)
 
-con <- dbConnect(drv,host=dbhost,port=dbport,dbname=dbname,user=dbuser,password=dbpass)
-
-suppressWarnings(
+  cat("Getting feeder data...\n")
+  #   incProgress(1/5)
   feeders_all <- dbGetQuery(con, statement = paste0("SELECT feeder_id, site_name, loc FROM feeders")) %>%
     load.format(tz = "") %>%
     mutate(site_name = factor(site_name))
-)
 
-sites_all <- feeders_all %>% 
-  group_by(site_name) %>% 
-  summarize(lon = mean(lon), lat = mean(lat)) %>%
-  mutate(site_name = factor(site_name))
+  cat("Getting site data...\n")
+  #    incProgress(2/5)
+  sites_all <- feeders_all %>%
+    group_by(site_name) %>%
+    summarize(lon = mean(lon), lat = mean(lat)) %>%
+    mutate(site_name = factor(site_name))
 
-suppressWarnings(
+  cat("Getting bird data...\n")
+  #  incProgress(3/5)
   birds_all <- dbGetQuery(con, statement = paste0("SELECT DISTINCT raw.visits.bird_id FROM raw.visits")) %>%
-    left_join(dbGetQuery(con, statement = paste0("SELECT bird_id, species, site_name, age, sex, tagged_on FROM birds")), by = "bird_id") %>% 
+    left_join(dbGetQuery(con, statement = paste0("SELECT bird_id, species, site_name, age, sex, tagged_on FROM birds")), by = "bird_id") %>%
     load.format(tz = "") %>%
     mutate(species = factor(species),
-           site_name = factor(site_name))
-)
+           site_name = factor(site_name),
+           bird_id = factor(bird_id))
 
-suppressWarnings(
-  counts <- dbGetQuery(con, 
-                     statement = paste0("SELECT raw.visits.bird_id, raw.visits.feeder_id, DATE(raw.visits.time), ",
-                                        "COUNT(*) ",
-                                        "FROM raw.visits ",
-                                        "GROUP BY DATE(raw.visits.time), raw.visits.feeder_id, raw.visits.bird_id"#,
-                     )) %>%
-  load.format(tz = "") %>%
-  left_join(birds_all[, c("site_name", "species", "bird_id")], by = "bird_id") %>%
-  mutate(date = as.Date(date),
-         count = as.numeric(count),
-         species = factor(species, levels = sort(unique(birds_all$species))),
-         site_name = factor(site_name, levels = sort(sites_all$site_name)),
-         bird_id = factor(bird_id, levels = sort(unique(birds_all$bird_id))),
-         feeder_id = factor(feeder_id, levels = sort(unique(feeders_all$feeder_id))))
-)
+  cat("Getting sample information...\n")
+  #    incProgress(4/5)
+  counts <- dbGetQuery(con,
+                       statement = paste0("SELECT raw.visits.bird_id, raw.visits.feeder_id, DATE(raw.visits.time), ",
+                                          "COUNT(*) ",
+                                          "FROM raw.visits ",
+                                          "GROUP BY DATE(raw.visits.time), raw.visits.feeder_id, raw.visits.bird_id"#,
+                       )) %>%
+    load.format(tz = "") %>%
+    left_join(birds_all[, c("site_name", "species", "bird_id")], by = "bird_id") %>%
+    mutate(date = as.Date(date),
+           count = as.numeric(count),
+           species = factor(species, levels = sort(unique(birds_all$species))),
+           site_name = factor(site_name, levels = sort(sites_all$site_name)),
+           bird_id = factor(bird_id, levels = sort(unique(birds_all$bird_id))),
+           feeder_id = factor(feeder_id, levels = sort(unique(feeders_all$feeder_id))))
+  dbDisconnect(con)
+})
 
-dbDisconnect(con)
+#  incProgress(5/5)
+  counts_sum <- bind_rows(
+    get_counts(counts, summarize_by = "site_name"),
+    get_counts(counts, summarize_by = "species"),
+    get_counts(counts, summarize_by = "date"),
+    get_counts(counts, summarize_by = "bird_id"),
+    get_counts(counts, summarize_by = "feeder_id"))
 
-
+#})
 
 startup <- function(x) {
   #require that input objects were at least created (first pass)
-  all(c("data_sitename", 
-        "data_species", 
+  all(c("data_site_name",
+        "data_species",
         "data_date"#,
-        #"data_birdid", 
-        #"data_feederid"
+        #"data_bird_id",
+        #"data_feeder_id"
   ) %in% names(x))
 }
 
 shinyServer(function(input, output, session) {
 
-    values <- reactiveValues(
-      data_submit = TRUE,
-      data = list(sitename = as.character(unique(counts$site_name)),
-                  species = as.character(unique(counts$species)),
-                  date = c(min(counts$date), max(counts$date)),
-                  birdid = unique(counts$bird_id),
-                  feederid = unique(counts$feeder_id)),
-      current = NULL)
-  
+
+  values <- reactiveValues(
+    reset = FALSE,
+    data = values_list(),
+    data_initial = values_list(),
+    data_previous = NULL,
+    hr_message = "",
+    map_hr = FALSE)
+
   ## Select Data
   source("select_data.R", local = TRUE)
-  
-  ## Load output data tables
+  source("map_data.R", local = TRUE)
+
+  ### Visualizations
+  source("map_animated.R", local = TRUE)
+  source("map_paths.R", local = TRUE)
+  source("map_static.R", local = TRUE)
+
+  ## Load transformation data tables
   source("output_data.R", local = TRUE)
-    
+
   ## Load reactive expressions
   source("reactive.R", local = TRUE)
   
-  ### Visualizations
-  source("map_data.R", local = TRUE)
-  source("map_animated.R", local = TRUE)
-  source("map_static.R", local = TRUE)
-  
+  ## Load inat
+  source("inat.R", local = TRUE)
+  source("homerange.R", local = TRUE)
+
   # Get initial Data
   #con <- dbConnect(drv,host=dbhost,port=dbport,dbname=dbname,user=dbuser,password=dbpass)
-  
+
   # dates_all <- reactive({
   #   req(values$map_data)
   #     con <- dbConnect(drv,host=dbhost,port=dbport,dbname=dbname,user=dbuser,password=dbpass)
-  #     d <- dbGetQuery(con, 
+  #     d <- dbGetQuery(con,
   #                statement = paste(
-  #                           "SELECT DISTINCT ON (raw.visits.time::date) 
+  #                           "SELECT DISTINCT ON (raw.visits.time::date)
   #                           raw.visits.time::date, feeders.site_name",
   #                           "FROM raw.visits, feeders",
   #                           "WHERE raw.visits.feeder_id = feeders.feeder_id")) %>%
@@ -140,7 +165,7 @@ shinyServer(function(input, output, session) {
   #     d
   # })
 
-  
+
   ## Look at birds
   output$img_birds <- renderText({
     req(imgs)
@@ -155,5 +180,5 @@ shinyServer(function(input, output, session) {
     paste("<img src='",temp,"' height = 300>")
     })
 
-  
+
 })
--- ui.R
+++ ui.R
@@ -7,7 +7,9 @@
 
 library(shiny)
 library(shinyjs)
+library(shinyBS)
 library(leaflet)
+library(ggvis)
 #library(shinyBS)
 
 shinyUI(fluidPage(
@@ -19,44 +21,50 @@ shinyUI(fluidPage(
              #################
              ## SETUP
              #################
-             tabPanel("Home",
+             tabPanel("Select Data",
                       fluidRow(div(img(src = "logo.jpg", width = 600), style="text-align: center;")),
                       fluidRow(
                         column(4,
                                h2("Data Selection"),
                                h3("Select Data"),
-                               uiOutput("UI_data_sitename"),
-                               uiOutput("UI_data_species"),
-                               uiOutput("UI_data_date"),
-                               actionButton("data_get", "Get Data"),                               
+                               uiOutput("UI_data"),
+                               bsButton("data_update", "Update Selection", style = "primary"),
+                               p(),
+                               bsButton("data_reset", "Reset inputs", style = "danger"),
+                               bsButton("data_get", "Get Data"),
+                               hr(),
                                h3("Advanced Options"),
                                actionButton("data_showadv", "Show Advanced Options"),
                                hidden(div(id = "advanced",
-                                          uiOutput("UI_data_birdid"),
-                                          uiOutput("UI_data_feederid"))),
-
-                               h2("Current Data Selection"),
-                               htmlOutput("data_current")
+                                          uiOutput("UI_data_adv")))
                         ),
                         column(8,
                                leafletOutput("map_data", height = 600),
-                               plotOutput("plot_data")
-                               #ggvisOutput("plot_data")
+                               plotOutput("plot_data_ggplot", 
+                                          brush = brushOpts(
+                                            id = "plot_data_brush",
+                                            direction = "x",
+                                            resetOnNew = TRUE),height = "100%"),
+                               div(strong("Drag and select a date range to further refine the data selection"), style = "text-align: center;")
+                               #ggvisOutput("plot_data_ggvis")
                         )
-                      ),
-                      fluidRow(
                       )
-             ),
-             #################
-             ## Visualization
-             #################
+              ),
+            # tabPanel("Current Data",
+            #          h1("Current Data Selection"),
+            #          htmlOutput("data_current"),
+            #          div("DATA TABLE")
+            # ),
+            #################
+            ## Visualization
+            #################
             tabPanel("Visualizations",
-                     column(3,
-                            h1("Data"), 
+                     column(4,
+                            h1("Data"),
                             radioButtons("anim_type", "Show data by:",
                                          choices = c("Individual visits" = "visits",
-                                                     "Total visits" = "t_visits", 
-                                                     "Avg. visits per bird" = "b_visits", 
+                                                     "Total visits" = "t_visits",
+                                                     "Avg. visits per bird" = "b_visits",
                                                      "Total birds" = "t_birds")),
                             h1("Animation"),
                             sliderInput("anim_speed", "Speed",
@@ -69,29 +77,26 @@ shinyUI(fluidPage(
                                         value = 12,
                                         post = " hour(s)"),
                             h3("To Do:"),
-                            div("Make the Date plot scale with the map?")
-                            #hr(),
-                            #h1("Potential Options:"),
-                            #submitButton(text = "Download Screenshot"),
-                            
-                            #submitButton(text = "Download Data")
+                            div("Make the Date plot scale with the map?"),
+                            hr(),
+                            h1("Potential Options:"),
+                            bsButton("data_dl_screenshot", "Download Screenshot", style = "success")
                      ),
-                     column(9,
-                            fluidRow(leafletOutput("map_time", height = 600)),
+                     column(8,
+                            tabsetPanel(type = "tabs",
+                                        tabPanel("Points",
+                                                 fluidRow(leafletOutput("map_points", height = 600))),
+                                        tabPanel("Paths",
+                                                 fluidRow(leafletOutput("map_paths", height = 600)))),
                             fluidRow(column(8, offset = 2, uiOutput("UI_anim_time"))),
                             fluidRow(column(8, offset = 1, plotOutput("plot_anim")))
-                            #,
-                            #fluidRow(
-                            #         DT::dataTableOutput("dt_points")
-                            #         )
                      )
-                     
             ),
-            
+
             # tabPanel("Paths (Static)",
             #          column(3,
-            #                 h1("Data"), 
-            #                 uiOutput("UI_static_birdid"),
+            #                 h1("Data"),
+            #                 uiOutput("UI_static_bird_id"),
             #                 h3("To Do:"),
             #                 div("Based on feedr package visualizations"),
             #                 div("Change feedr functions to have map and layers separate, so doesn't refresh entire map for each bird")
@@ -111,28 +116,80 @@ shinyUI(fluidPage(
             #          fluidRow(
             #                   #DT::dataTableOutput("dt_paths")
             #                   )
-            #                 
+            #
             # ),
             #################
             ## DATA
             #################
             tabPanel("Birds",
-                     fluidRow(column(6, 
+                     fluidRow(column(6,
                                      h3("Click on a row for more information"),
                                      div("Presumably, we could replace images these with the actual pictures that are stored in the database? Right now they all come from wikipedia")),
                               column(6, htmlOutput("img_birds"))),
                      fluidRow(DT::dataTableOutput("dt_birds"))
             ),
-            tabPanel("Raw Data",
-                      DT::dataTableOutput("dt_data")
+            tabPanel("Data Transformations",
+                     column(2,
+                            "This is where you can download your data",
+                            br(),
+                            bsButton("data_dl_all", "Download All", style = "success")
+                            ),
+                     column(10,
+                            tabsetPanel(type = "tabs",
+                                        tabPanel("Raw Data",
+                                                 br(), bsButton("data_dl_raw", "Download Raw Data", style = "success"), br(),
+                                                 DT::dataTableOutput("dt_data")),
+                                        tabPanel("Visits Data",
+                                                 br(), bsButton("data_dl_visits", "Download Visits Data", style = "success"), br(),
+                                                 DT::dataTableOutput("dt_v")),
+                                        tabPanel("Feeding Data",
+                                                 br(), bsButton("data_dl_feeding", "Download Feeding Data", style = "success"), br(),
+                                                 DT::dataTableOutput("dt_f")),
+                                        tabPanel("Movement Data",
+                                                 br(), bsButton("data_dl_movements", "Download Movements Data", style = "success"), br(),
+                                                 DT::dataTableOutput("dt_m"))
+                            )
+                     )
             ),
-            
-            tabPanel("Visits Data",
-                     DT::dataTableOutput("dt_v")
+            tabPanel("Home Range (testing)",
+                     column(4,
+                            h3("Homerange calculation"),
+                            uiOutput("UI_hr"),
+                            uiOutput("UI_hr_bandwidth"),
+                            uiOutput("UI_hr_plot"),
+                            bsButton("homerange", "Calculate and Display", style = "success"),
+                            hr(),
+                            fluidRow(div(strong(textOutput("hr_message")),  
+                                         style="text-align:center; padding: 10px"),
+                                     style = "border: 1px solid silver; margin-right: 7px")
+                          
+                     ),
+                     column(8,
+                            fluidRow(leafletOutput("map_hr", height = 600))
+                            # fluidRow(column(8, offset = 2, uiOutput("UI_inat_time")))
+                     )
+                     
+            ),
+            tabPanel("iNaturalist (testing)",
+                     column(4,
+                            h1("Which Data?"),
+                            radioButtons("inat_species", "Species:",
+                                         choices = c("House Finch" = "Haemorhous mexicanus",
+                                                     "Mountain Chickadee" = "Poecile gambeli",
+                                                     "Dark-eyed Junco" = "Junco hyemalis")),
+                            dateRangeInput("inat_date", label = "Observation Dates",
+                                           start = as.Date("2010-01-01"),
+                                           end = Sys.Date()),
+                            bsButton("inat_data", "Get Data from iNaturalist", style = "success"),
+                            hr(),
+                            h1("Photos"),
+                            htmlOutput("inat_photo")
+                     ),
+                     column(8,
+                            fluidRow(leafletOutput("map_inat", height = 600))#,
+                           # fluidRow(column(8, offset = 2, uiOutput("UI_inat_time")))
+                     )
+                  
             )
-                   #fluidRow(
-                   #         DT::dataTableOutput("dt_points")
-                   #         )
-          
   )
 ))
