7a677ff6f82fbff2aa7a482398fb9ed91a0b07d6
steffilazerte <steffi@steffi.ca>
2016-06-22 16:12:14 -0700

    Add to disp() tests

---
+++ tests/testthat/test_disp.R
@@ -0,0 +1,66 @@
+library(feedr)
+library(magrittr)
+context("Transformations to displacements")
+
+# disp()
+test_that("disp() returns appropriate, non-empty dataframe", {
+
+  ## Errors
+  expect_error(d <- visits(finches) %>% disp(.))
+  expect_silent(d <- visits(finches) %>% disp(., bw = 300))
+
+  ## Format
+  expect_is(d1 <- d[["displacements"]], "data.frame")
+  expect_is(d2 <- d[["summaries"]], "data.frame")
+  expect_is(d3 <- d[["interactions"]], "data.frame")
+
+  expect_length(d1, 10)
+  expect_length(d2, 4)
+  expect_length(d3, 3)
+
+  expect_equal(nrow(d1), 24)
+  expect_equal(nrow(d2), 5)
+  expect_equal(nrow(d3), 20)
+
+  expect_equal(sum(is.na(d1)), 0)
+  expect_equal(sum(is.na(d2)), 0)
+  expect_equal(sum(is.na(d3)), 0)
+
+  expect_match(names(d1)[1:3], "^bird_id$|^left$|^arrived$|^feeder_id$|^role$")
+  expect_is(d1$bird_id, "factor")
+  expect_is(d1$feeder_id, "factor")
+  expect_is(d1$left, "POSIXct")
+})
+
+test_that("disp() has correct spacing", {
+  for(b in c(15, 30, 100, 300, 1000)) {
+    d <- visits(finches) %>% disp(., bw = b) %>% .$displacements
+    expect_true(all(as.numeric(difftime(d$arrived, d$left, units = "secs")) <= b))
+  }
+})
+
+test_that("disp() returns correct data", {
+  d <- visits(finches) %>% disp(., bw = 300)
+  d1 <- d[["displacements"]]
+  d2 <- d[["summaries"]]
+  d3 <- d[["interactions"]]
+
+  ## Data
+  expect_equal(unique(d1$bird_id[1]), factor("041868D861", levels = c("041868D396", "041868D861", "041868FF93", "062000043E", "06200004F8", "0620000514")))
+  expect_equal(d1$feeder_id[1], factor(c("2200"), levels = c("2100", "2200", "2400", "2700")))
+  expect_equal(d1$left[1], as.POSIXct(c("2016-01-28 12:34:28"), tz = "America/Vancouver"))
+  expect_equal(d1$bird_n[1], 6)
+  expect_equal(d1$feeder_n[1], 4)
+  expect_equal(round(d1$lon[2], 4), -120.3612)
+  expect_equal(round(d1$lat[2], 5), 50.66778)
+  expect_equal(d1$role[1], "displacer")
+  expect_equal(d1$species[1], "House Finch")
+
+  expect_equal(d2$p_win[1], 1.00)
+  expect_equal(sum(d3$n == 0), 12)
+})
+
+test_that("disp() pass", {
+  expect_length(visits(finches) %>% disp(., bw = 30, pass = FALSE) %>% .$displacements, 5)
+  expect_length(visits(finches) %>% disp(., bw = 30, pass = TRUE) %>% .$displacements, 10)
+})
