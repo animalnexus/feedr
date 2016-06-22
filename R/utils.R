dc4ab6f047f71435f5962e33140478fbe5144461
steffilazerte <steffi@steffi.ca>
2016-06-22 16:10:10 -0700

    Remove col.order function

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,1 @@
-
-col.order <- function(d, n){
-  d <- d[, c(n, names(d)[!(names(d) %in% n)])]
-  return(d)
-}
-
 mp <- function(x) paste0(sort(unlist(strsplit(as.character(x), "_"))), collapse = "_")
