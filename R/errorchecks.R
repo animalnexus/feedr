4774f7c08f46eda8858e03ad7763b2d195c90baf
steffilazerte <steffi@steffi.ca>
2016-07-12 12:41:30 -0700

    Remove call returns add info to check_name.

--- R/errorchecks.R
+++ R/errorchecks.R
@@ -1,16 +1,16 @@
 
-check_name <- function(d, n = c("bird_id", "feeder_id")) {
-  if(!all(n %in% names(d))) stop(paste0("Required columns aren't present. Require: ", paste0("'", n, "'", collapse = ", ")))
+check_name <- function(d, n = c("bird_id", "feeder_id"), type = "visit") {
+  if(!all(n %in% names(d))) stop(paste0("You should be using '", type, "' data. Required columns aren't present. Require: ", paste0("'", n, "'", collapse = ", ")), call. = FALSE)
 }
 
 check_time <- function(d, n = c("start", "end"), internal = TRUE) {
   if(!all(sapply(d[, n], class) == c("POSIXct", "POSIXt"))) {
-    stop(paste0("Columns ", paste0("'", n, "'", collapse = ", "), " must be in R's date/time formating (POSIXct).", ifelse(internal == TRUE, " This data frame should have been created with a feedr function. Have you changed the values since they were created?", " Consider as.POSIXct() and strptime() or lubridate::parse_date_time().")))
+    stop(paste0("Columns ", paste0("'", n, "'", collapse = ", "), " must be in R's date/time formating (POSIXct).", ifelse(internal == TRUE, " This data frame should have been created with a feedr function. Have you changed the values since they were created?", " Consider as.POSIXct() and strptime() or lubridate::parse_date_time().")), call. = FALSE)
   }
 }
 
 check_indiv <- function(d) {
-  if(length(unique(d$bird_id)) > 1) stop("This function is only designed to be run on one individual at a time. Consider using the ddply() function from the plyr package, or the do() function from the dplyr package to apply this function to all birds.")
+  if(length(unique(d$bird_id)) > 1) stop("This function is only designed to be run on one individual at a time. Consider using the ddply() function from the plyr package, or the do() function from the dplyr package to apply this function to all birds.", call. = FALSE)
 }
 
 check_format <- function(d, map = FALSE, disp = FALSE) {
