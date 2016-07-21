92c2a802da76e06c338c041b50807a681e743782
steffilazerte <steffi@steffi.ca>
2016-07-21 11:02:52 -0700

    Update demo

--- demo/demo-leaflet.R
+++ demo/demo-leaflet.R
@@ -3,13 +3,23 @@ library(feedr)
 library(plyr)
 
 v <- visits(chickadees)
+v <- v[v$experiment == "exp1", ] ## Only look at first experiment
+
 v <- ddply(v, "experiment", transform, bird_n_exp = length(unique(bird_id)))
 m <- ddply(v, "bird_id", move)
 f <- ddply(v, "bird_id", feeding)
+
+## Feeder Use (u)
 f.all <- ddply(f, "feeder_id", summarise,
-               feed_length = sum(feed_length) / bird_n_exp[1])
-m.all <- ddply(m, "move_path", summarise,
+               amount = sum(feed_length) / bird_n_exp[1])
+
+## Movements along paths (p)
+m.all <- ddply(m, c("feeder_id", "move_path"), summarise,
                path_use = length(move_path) / bird_n_exp[1] / 2)
+
+## Locations (l)
 l <- unique(f[, c("feeder_id", "lat", "lon")])
 l <- rbind(l, data.frame(feeder_id = "exp1-GR11", lat = 53.89304, lon = -122.81827))
-map.leaflet(f = f.all, m = m.all, locs = l)
+
+## Map
+map_leaflet(u = f.all, p = m.all, locs = l)
