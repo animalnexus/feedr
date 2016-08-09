ac71b6e77103c430c4d83cfc5cd144048b31dc05
steffilazerte <steffi@steffi.ca>
2016-08-09 11:42:59 -0700

    Add project info

--- inst/shiny-examples/birdmoves/ui.R
+++ inst/shiny-examples/birdmoves/ui.R
@@ -153,11 +153,35 @@ shinyUI(fluidPage(
                      p(),
                      "If you're interested in contruting functionality and/or coding to this project, check out our project on ", HTML(paste0("<a href = 'http://github.org/steffilazerte/feedr/' target = 'blank'>GitHub <img src = 'GitHub-Mark-32px.png'></a>")), "We welcome all involvement!",
 
-                     h1("People"),
-                     "Project Blurbs and Pictures",
-                     "People, etc."
+                     h1("People and Individual Projects"),
+
+                     h3("TRU campus monitoring"),
+                     div(strong("Thompson Rivers University")),
+                     div(strong("Matt Reudink, Mark Paetkau, David J. Hill, Jackson Kusack, Alistair Sutter, Jerin Roberts, Jamie Shippit, Brandon Turner, Steffi LaZerte")),
+                     div("Wifi enabled feeders automatically upload data on feeder use of common city birds (house finches, mountain chickadees, etc.). We're currently examining barriers to movement in an urban setting"),
+
+                     h3("UNBC gap-crossing in chickadees"),
+                     div(strong("University of Northern British Columbia")),
+                     div(strong("Ken Otter, Jacob Baily, Steffi LaZerte")),
+                     div("Habitats gaps have been well documented to act as habitat barriers to bird movement, negatively effecting the dispersal of juveniles, and reducing genetic connectivity between populations. Our work uses a novel approach (radio-frequency identification - RFID) to assess the permeability of habitat gaps, and investigate what factors influence avian gap crossing decisions. Using bird feeders outfitted with RFID readers, we tracked the movement patterns of resident, wintering black-capped chickadees individually banded with passive integrated transponder (PIT) tags. Preliminary results support the prediction that gaps are perceived as movement barriers; bird movements between feeders on opposite sides of gaps were reduced compared to feeders on the same sides of habitat gaps."),
+
+                     h3("SLU behaviour and movements of wintering birds"),
+                     div(strong("Swedish University for Agricultural Sciences (SLU)")),
+                     div(strong("Adriaan De Jong, Bo-Sören Wiklund, Holger Dettki, Ola Engman and students from Dragonskolan College")),
+
+                     div("At SLU campus in northern Sweden, studies of behaviour and movements of wintering birds are combined with the development of rugged high-tech RFID feeder/reader units for long-term monitoring under harsh climatic conditions."),
+
+                     h3("OSU Community ecology of tropical pollinators"),
+                     div(strong("Oregon State University")),
+                     div(strong("Matt Betts, Evan Jackson")),
+                     div("Pollination is essential to human wellbeing, as well as fundamental to maintenance of biological diversity. Research showing large-scale declines of plants and pollinators highlights the concern that pollination is an ecosystem service at risk. Habitat loss and fragmentation have been implicated as primary drivers of pollination declines, but these observations contradict most model predictions indicating that pollinator networks should be resilient. Very recent theoretical models predict that if ‘keystone’ species are lost, pollination webs may be more vulnerable, resulting in sudden network collapse. However, lack of experimental studies examining the consequences of plant or pollinator extinction means that we know little about whether such collapses occur in nature."),
+                     div("This research will focus on the idea that plant-pollinator interactions can be lost if pollinator movement is restricted due to landscape fragmentation. The tropical hummingbird-plant system is ideal for this because the large size of hummingbirds in relation to other pollinators enables tracking of movements across whole landscapes."),
+                     div("This research will address two overarching questions: (1) What are the consequences of keystone plant decline and extinction to the broader pollination network? (2) Can pollinator movement and landscape connectivity buffer pollination networks against network collapse?"),
+                     div("To address these questions the research team will experimentally remove H. tortuosa in isolated and connected forest fragments to determine whether altered densities of this species affects (a) hummingbird movement, (b) plant reproduction and genetic diversity, and (c) the structure of the pollination network. Hummingbird movement will be quantified for all species in the network using Radio-Frequency Identification Devices (RFID). This method, already successfully demonstrated in this project, will enable observations of the entire hummingbird pollinator community at an unprecedented temporal resolution (seconds) and spatial extent (1000 m radius landscapes).")
+
             ),
             tabPanel("References",
+                     "Coming soon!",
                      h1("FAQ"),
                      h1("Caveats and Disclaimers"),
                      h1("Tutorials"),
