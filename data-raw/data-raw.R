00ea8ecf5777bd2c5130f3ff4066b2edf27a2cad
steffilazerte <steffi@steffi.ca>
2016-11-01 13:52:47 -0300

    Change chickadee data example

---
+++ data-raw/data-raw.R
@@ -0,0 +1,17 @@
+library(dplyr)
+finches <- get.data(start = "2016-01-28", end = "2016-01-30")
+
+write.csv(finches, "./data-raw/finches.csv", row.names = FALSE)
+devtools::use_data(finches, overwrite = TRUE)
+
+
+chickadees <- load_raw_all(system.file("extdata", "raw", package = "feedr"), extra_pattern = "exp[0-9]{1}", extra_name = "experiment")
+chickadees$feeder_id <- paste0(chickadees$experiment, "-", chickadees$feeder_id)  ##Make feeder id unique
+
+locs <- read.csv(system.file("extdata", "raw", "feeder_index.csv", package = "feedr"))
+locs$feeder_id <- paste0(locs$experiment, "-", locs$feeder_name)
+
+chickadees <- merge(chickadees, locs[, c("feeder_id", "lat", "lon")], by = "feeder_id", all.x = TRUE, all.y = FALSE)
+
+write.csv(chickadees, "./data-raw/chickadees.csv", row.names = FALSE)
+devtools::use_data(chickadees, overwrite = TRUE)
--- tests/testthat/test_load.R
+++ tests/testthat/test_load.R
@@ -19,32 +19,32 @@ test_that("load_web loads and formats data correctly", {
 
 # load_raw() - feeder_id from file name
 test_that("load_raw loads and formats data correctly - feeder id from file name", {
-  f <- system.file("extdata", "raw", "exp1", "GR12DATA_2015_12_24.TXT", package = "feedr")
+  f <- system.file("extdata", "raw", "exp2", "GR10DATA_2016_01_16.TXT", package = "feedr")
   expect_is(r <- load_raw(r_file = f), "data.frame")
   expect_match(names(r)[1:3], "^bird_id$|^time$|^feeder_id$")
   expect_is(r$bird_id, "character")
   expect_is(r$feeder_id, "character")
   expect_is(r$time, "POSIXct")
 
-  expect_equal(r$bird_id[1], "062000014F")
-  expect_equal(r$feeder_id[1], "GR12")
-  expect_equal(r$time[1], as.POSIXct("2015-12-15 11:16:08", tz = "America/Vancouver"))
-  expect_equal(load_raw(f, tz_disp = "America/Toronto")$time[1], as.POSIXct("2015-12-15 14:16:08", tz = "America/Toronto"))
+  expect_equal(r$bird_id[2], "062000014F")
+  expect_equal(r$feeder_id[2], "GR10")
+  expect_equal(r$time[2], as.POSIXct("2016-01-11 12:00:54", tz = "America/Vancouver"))
+  expect_equal(load_raw(f, tz_disp = "America/Toronto")$time[2], as.POSIXct("2016-01-11 15:00:54", tz = "America/Toronto"))
 })
 
 # load_raw() - feeder_id from first line
 test_that("load_raw loads and formats data correctly - feeder id from first line", {
-  f <- system.file("extdata", "raw", "exp1", "GR12DATA_2015_12_24.TXT", package = "feedr")
+  f <- system.file("extdata", "raw", "exp2", "GR10DATA_2016_01_16.TXT", package = "feedr")
   expect_is(r <- load_raw(r_file = f, feeder_id_loc = "firstline"), "data.frame")
   expect_match(names(r)[1:3], "^bird_id$|^time$|^feeder_id$")
   expect_is(r$bird_id, "character")
   expect_is(r$feeder_id, "character")
   expect_is(r$time, "POSIXct")
 
-  expect_equal(r$bird_id[1], "062000014F")
-  expect_equal(r$feeder_id[1], "GR12")
-  expect_equal(r$time[1], as.POSIXct("2015-12-15 11:16:08", tz = "America/Vancouver"))
-  expect_equal(load_raw(f, feeder_id_loc = "firstline", tz_disp = "America/Toronto")$time[1], as.POSIXct("2015-12-15 14:16:08", tz = "America/Toronto"))
+  expect_equal(r$bird_id[2], "062000014F")
+  expect_equal(r$feeder_id[2], "GR10")
+  expect_equal(r$time[2], as.POSIXct("2016-01-11 12:00:54", tz = "America/Vancouver"))
+  expect_equal(load_raw(f, feeder_id_loc = "firstline", tz_disp = "America/Toronto")$time[2], as.POSIXct("2016-01-11 15:00:54", tz = "America/Toronto"))
 })
 
 # load_raw() empty file
@@ -65,10 +65,10 @@ test_that("load_raw_all loads and formats data correctly", {
   expect_is(load$feeder_id, "factor")
   expect_is(load$time, "POSIXct")
 
-  expect_equal(load$bird_id[1], factor("0620000006", levels =  c("0000000000", "03000314F9", "0620000006", "0620000062", "06200000F7", "062000014F", "06200001F0", "06200002E7", "0620000318", "0620000380", "0620000384", "062000038D", "062000038F", "0620000392", "062000039D", "06200003A7", "06200003B4", "06200003C3", "06200003DE", "06200003E0", "06200003F3", "0620000400", "0620000418", "0620000453", "06200004A9", "06200004BB", "06200004BE", "06200004BF", "06200004E4", "0620000503", "0620000525", "0700EDAB15", "0700EE022B", "0700EE0E42", "0700EE1504")))
+  expect_equal(load$bird_id[1], factor("0000000000", levels =  c("0000000000", "011017A536", "011017A605", "03000314F9", "0620000062", "062000014F", "06200001F0", "06200002E7", "0620000380", "062000038D", "062000038F", "0620000392", "06200003A7", "06200003B4", "06200003C3", "06200003F3", "0620000400", "0620000418", "06200004A9", "06200004BB", "06200004BE", "06200004BF", "06200004E4", "0620000525", "07008D9E08", "0700ED9E0E", "0700EDAB15", "0700EDF012", "0700EDF015", "0700EE022B", "0700EE0E42", "0700EE1461", "0700EE1467", "0700EE147F", "0700EE1504", "0700EE19CE", "0700EE2B10", "0700EE2B11")))
   expect_equal(load$feeder_id[1], factor("GR10", levels = c("GR10", "GR11", "GR12", "GR13")))
-  expect_equal(load$time[1], as.POSIXct("2015-12-05 12:57:48", tz = "America/Vancouver"))
-  expect_equal(load2$time[1], as.POSIXct("2015-12-05 15:57:48", tz = "America/Toronto"))
+  expect_equal(load$time[1], as.POSIXct("2016-01-11 11:34:08", tz = "America/Vancouver"))
+  expect_equal(load2$time[1], as.POSIXct("2016-01-11 14:34:08", tz = "America/Toronto"))
 })
 
 # dl_data()
