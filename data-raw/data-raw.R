18f580db93484592060a1f59a0b2fc9207f0042d
steffilazerte <steffi@steffi.ca>
2016-11-07 11:53:24 -0400

    Update external data organization

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,10 +1,12 @@
 library(dplyr)
+
+## Finches
 finches <- get.data(start = "2016-01-28", end = "2016-01-30")
 
 write.csv(finches, "./data-raw/finches.csv", row.names = FALSE)
 devtools::use_data(finches, overwrite = TRUE)
 
-
+### Chickadees
 chickadees <- load_raw_all(system.file("extdata", "raw", package = "feedr"), extra_pattern = "exp[0-9]{1}", extra_name = "experiment")
 chickadees$feeder_id <- paste0(chickadees$experiment, "-", chickadees$feeder_id)  ##Make feeder id unique
 
@@ -15,3 +17,7 @@ chickadees <- merge(chickadees, locs[, c("feeder_id", "lat", "lon")], by = "feed
 
 write.csv(chickadees, "./data-raw/chickadees.csv", row.names = FALSE)
 devtools::use_data(chickadees, overwrite = TRUE)
+
+## Copy Data to Website Data folder
+system("cp -r ./inst/extdata/raw/* ./docs/Data/Raw")
+system("cp ./inst/extdata/* ./docs/Data/")
