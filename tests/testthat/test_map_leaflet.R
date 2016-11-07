0ebcdd6b6ae21f5de604700525850340b6b56ad5
steffilazerte <steffi@steffi.ca>
2016-11-07 14:40:08 -0400

    Update tests

--- tests/testthat/test_map_leaflet.R
+++ tests/testthat/test_map_leaflet.R
@@ -10,21 +10,15 @@ test_that("maps_leaflet_base return maps", {
 })
 
 test_that("ggmap and leaflet map return map", {
-  f.indiv <- visits(finches) %>%
-    dplyr::group_by(bird_id) %>%
-    dplyr::do(feeding(., bw = 15)) %>%
+  f.indiv <- feeding(visits(finches), bw = 15) %>%
     dplyr::group_by(bird_id, feeder_id, lat, lon) %>%
     dplyr::summarize(amount = sum(feed_length))
 
-  m.indiv <- visits(finches) %>%
-    dplyr::group_by(bird_id) %>%
-    dplyr::do(move(.)) %>%
+  m.indiv <- move(visits(finches)) %>%
     dplyr::group_by(bird_id, feeder_id, move_path) %>%
     dplyr::summarize(path_use = length(move_path))
 
-  m.error <- visits(finches) %>%
-    dplyr::group_by(bird_id) %>%
-    dplyr::do(move(.)) %>%
+  m.error <- move(visits(finches)) %>%
     dplyr::group_by(bird_id, move_path) %>%
     dplyr::summarize(path_use = length(move_path))
 
