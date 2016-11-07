828e1a77a0b0f197445aa40094a4d9996a9b32d3
steffilazerte <steffi@steffi.ca>
2016-11-07 13:58:03 -0400

    Remove load_web() function

--- tests/testthat/test_load.R
+++ tests/testthat/test_load.R
@@ -1,22 +1,6 @@
 library(feedr)
 context("Loading files")
 
-# load_web()
-test_that("load_web loads and formats data correctly", {
-  load <- load_web("../data/test_load_web.csv")
-  load2 <- load_web("../data/test_load_web.csv", tz_disp = "America/Toronto")
-  expect_is(load, "data.frame")
-  expect_match(names(load)[1:3], "^bird_id$|^time$|^feeder_id$")
-  expect_is(load$bird_id, "factor")
-  expect_is(load$feeder_id, "factor")
-  expect_is(load$time, "POSIXct")
-
-  expect_equal(load$bird_id[1], factor("0620000514", levels = c("041868D396", "041868D861", "041868FF93", "062000043E", "06200004F8", "0620000514")))
-  expect_equal(load$feeder_id[1], factor("2200", levels = c("2100", "2200", "2400", "2700")))
-  expect_equal(load$time[1], as.POSIXct("2016-01-28 12:34:25", tz = "America/Vancouver"))
-  expect_equal(load2$time[1], as.POSIXct("2016-01-28 15:34:25", tz = "America/Toronto"))
-})
-
 # load_raw() - feeder_id from file name
 test_that("load_raw loads and formats data correctly - feeder id from file name", {
   f <- system.file("extdata", "raw", "exp2", "GR10DATA_2016_01_16.TXT", package = "feedr")
