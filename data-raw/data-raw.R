855dd169a257237ec7249d5411ada8fa79e2f739
Stefanie LaZerte <steffi@steffi.ca>
2017-02-10 11:41:34 -0400

    Update data, save to extdata

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -10,16 +10,20 @@ finches_lg <- dl_data(start = "2015-09-01",
          ! (animal_id %in% c("041868D100", "041868D396"))) %>%
   mutate(species = "House Finch") %>%
   arrange(animal_id, time, logger_id) %>%
-  droplevels() %T>%
-  write.csv("./data-raw/finches_lg.csv", row.names = FALSE)
+  droplevels()
+
+write.csv("./data-raw/finches_lg.csv", row.names = FALSE)
+write.csv("./inst/extdata/finches_lg.csv", row.names = FALSE)
 devtools::use_data(finches_lg, overwrite = TRUE)
 
 finches <- dl_data(start = "2016-01-28", end = "2016-01-30",
                    animal_details = c("species", "sex")) %>%
   filter(species == "HOFI") %>%
   mutate(species = "House Finch") %>%
-  droplevels() %T>%
-  write.csv("./data-raw/finches.csv", row.names = FALSE)
+  droplevels()
+
+write.csv(finches, "./data-raw/finches.csv", row.names = FALSE)
+write.csv(finches, "./inst/extdata/finches.csv", row.names = FALSE)
 devtools::use_data(finches, overwrite = TRUE)
 
 
@@ -34,8 +38,11 @@ chickadees <- load_raw_all(system.file("extdata", "raw", package = "feedr"), ext
          (date > as.Date("2016-01-31") & date <= as.Date("2016-02-15"))) %>%
   left_join(locs[, c("logger_id", "lat", "lon")], by = "logger_id") %>%
   check_ids(ids = data.frame(animal_id = "0000000000", species = "error")) %>%
-  droplevels() %T>%
-  write.csv("./data-raw/chickadees.csv", row.names = FALSE)
+  load_format() %>%
+  droplevels()
+
+write.csv(chickadees, "./data-raw/chickadees.csv", row.names = FALSE)
+write.csv(chickadees, "./inst/extdata/chickadees.csv", row.names = FALSE)
 devtools::use_data(chickadees, overwrite = TRUE)
 
 ## Copy Data to Website Data folder
