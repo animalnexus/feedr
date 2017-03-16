6020266f4f61eff489587bcd9602bb12ec3558be
Stefanie LaZerte <steffi@steffi.ca>
2017-03-16 10:44:46 -0300

    Clean up

--- R/mod_trans.R
+++ R/mod_trans.R
@@ -218,7 +218,7 @@ mod_trans <- function(input, output, session, r, settings, verbose = FALSE) {
 
   # Downloads ---------------------------------------------------
 
-  # Setup
+  # Download Individual
   observeEvent(trans$raw, {
     lapply(1:nrow(types), function(x) {
       x <- types[x, ]
