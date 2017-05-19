945b98a07f14eb501889fb2e5b6ee80815eb9eca
Stefanie LaZerte <steffi@steffi.ca>
2017-05-19 12:41:59 -0300

    Use png to compare map_ggmap output

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,15 +3,18 @@ library(magrittr)
 expect_equal_to_ggplot_reference <- function(object, file, info = NULL) {
 
   if (!file.exists(file)) {
-    saveRDS(object, file)
+    ggplot2::ggsave(file, object, width = 5, height = 5, dpi = 300)
     succeed()
   }
   else {
-    reference <- readRDS(file)
-    object <- object[names(object)[names(object) != "plot_env"]]
-    reference <- reference[names(reference)[names(reference) != "plot_env"]]
-    comp <- compare(object, reference)
-    expect(comp$equal, sprintf("Not equal.\n%s", comp$message), info = info)
+    ggplot2::ggsave("temp.png", object, width = 5, height = 5, dpi = 300)
+    reference <- png::readPNG(file)
+    object <- png::readPNG("temp.png")
+    if(length(reference) == length(object)) {
+      diff <- 100 * sum(object == reference) / length(reference)
+    } else diff <- 0
+    expect_gt(diff, 99, "Figures not equal")
+    file.remove("temp.png")
   }
   invisible(object)
 }
--- tests/testthat/test_c06_map_ggmap.R
+++ tests/testthat/test_c06_map_ggmap.R
@@ -5,13 +5,13 @@ test_that("map_ggmap() returns summary maps", {
                                 m = move(visits(finches)),
                                 summary = "sum"), NA)
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_sum_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_sum_map.png")
 
   expect_error(map <- map_ggmap(p = presence(visits(finches)),
                                 m = move(visits(finches)),
                                 summary = "sum_indiv"), NA)
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_sum_indiv_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_sum_indiv_map.png")
 
   p2 <- presence(visits(finches)) %>%
     dplyr::group_by(logger_id, lat, lon) %>%
@@ -23,7 +23,21 @@ test_that("map_ggmap() returns summary maps", {
 
   expect_message(expect_error(map <- map_ggmap(p = p2, m = m2), NA))
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_none_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_none_map.png")
+
+  # Summary by hand is equal to function summary
+  p2 <- presence(visits(finches)) %>%
+    dplyr::group_by(logger_id, lat, lon) %>%
+    dplyr::summarize(amount = sum(length)/animal_n[1])
+
+  m2 <- move(visits(finches)) %>%
+    dplyr::group_by(logger_id, move_path, lat, lon) %>%
+    dplyr::summarize(path_use = length(move_path)/animal_n[1])
+
+  expect_message(expect_error(map <- map_ggmap(p = p2, m = m2), NA))
+  expect_is(map, c("gg", "ggplot"))
+  expect_equal_to_ggplot_reference(map, "ggmap_sum_indiv_map.png")
+
 })
 
 test_that("map_ggmap() returns map of individuals", {
@@ -38,15 +52,15 @@ test_that("map_ggmap() returns map of individuals", {
 
   expect_message(expect_error(map <- map_ggmap(p = p_indiv, m = m_indiv), NA), "(Some animals)|(Map from URL)|(You have specified)")
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_indiv1_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_indiv1_map.png")
   suppressMessages({map2 <- map_ggmap(p = presence(visits(finches)),
                                       m = move(visits(finches)),
                                       summary = "indiv")})
-  expect_equivalent(map[-8], map2[-8]) #Don't compare plot environment
+  expect_equal_to_ggplot_reference(map, "ggmap_indiv1_map.png")
 
   expect_message(expect_error(map <- map_ggmap(p = p_indiv, m = m_indiv, which = c("06200004F8")), NA), "(Map from URL)|(You have specified)")
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_indiv2_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_indiv2_map.png")
 })
 
 test_that("map_ggmap() scale, pal, title", {
@@ -58,7 +72,7 @@ test_that("map_ggmap() scale, pal, title", {
                                                m_pal = c("blue", "white"), p_pal = c("green", "yellow")),
                               NA))
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_args1_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_args1_map.png")
 })
 
 test_that("map_ggmap() maptype, mapsource, zoom", {
@@ -69,7 +83,7 @@ test_that("map_ggmap() maptype, mapsource, zoom", {
                                                mapsource = "google",
                                                zoom = 15), NA))
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_args2_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_args2_map.png")
 
   expect_message(expect_error(map <- map_ggmap(p = presence(visits(finches)),
                                                m = move(visits(finches)),
@@ -78,5 +92,5 @@ test_that("map_ggmap() maptype, mapsource, zoom", {
                                                mapsource = "stamen",
                                                zoom = 16), NA))
   expect_is(map, c("gg", "ggplot"))
-  expect_equal_to_ggplot_reference(map, "ggmap_args3_map.rds")
+  expect_equal_to_ggplot_reference(map, "ggmap_args3_map.png")
 })
