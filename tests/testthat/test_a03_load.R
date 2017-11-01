1c9feda43df756b666a7f110ed7ffcc16474d5c7
Stefanie LaZerte <steffi@steffi.ca>
2017-11-01 15:03:58 -0500

    Explicitly add date timezone. Fixes #1 - Order date column - Add tests to catch

--- tests/testthat/test_a03_load.R
+++ tests/testthat/test_a03_load.R
@@ -11,7 +11,7 @@ test_that("load_format() loads and formats data correctly", {
   current_tz_nodst <- tz_offset(current_tz, tz_name = TRUE)
 
   expect_is(r <- load_format(f), "data.frame")
-  expect_match(names(r)[1:3], "^animal_id$|^time$|^logger_id$|^date$")
+  expect_match(names(r)[1:4], "^animal_id$|^time$|^logger_id$|^date$")
   expect_is(r$animal_id, "factor")
   expect_is(r$logger_id, "factor")
   expect_is(r$time, "POSIXct")
@@ -21,6 +21,7 @@ test_that("load_format() loads and formats data correctly", {
   expect_equal(r$logger_id[1], chickadees$logger_id[1])
   expect_equal(r$time[1], as.POSIXct("2016-01-11 10:48:49", tz = current_tz_nodst))
   expect_equal(r$date[1], as.Date("2016-01-11"))
+  expect_equal(substr(r$time, 1, 10), as.character(r$date))
 
   # Current tz
   expect_equal(load_format(f)$time[1], as.POSIXct("2016-01-11 10:48:49", tz = current_tz_nodst))
@@ -50,7 +51,8 @@ test_that("load_format fixes names", {
   expect_message(load_format(r1), "Renaming column 'latitude' to 'lat'")
   expect_message(load_format(r1), "Renaming column 'TIME' to 'time'")
 
-  expect_named(suppressMessages(load_format(r1)), c("animal_id", "time", "logger_id", "specIes", "SEX", "lon", "lat", "date"))
+  expect_named(suppressMessages(load_format(r1)),
+               c("animal_id", "date", "time", "logger_id", "specIes", "SEX", "lon", "lat"))
 })
 
 
@@ -62,13 +64,16 @@ test_that("load_raw loads and formats data correctly - logger id from file name"
   expect_silent(r <- load_raw(r_file = f, verbose = FALSE))
   expect_is(r, "data.frame")
 
-  expect_match(names(r)[1:3], "^animal_id$|^time$|^logger_id$")
+  expect_match(names(r)[1:4], "^animal_id$|^date$|^time$|^logger_id$")
   expect_is(r$animal_id, "character")
   expect_is(r$logger_id, "character")
   expect_is(r$time, "POSIXct")
+  expect_is(r$date, "Date")
 
   expect_equal(r$animal_id[2], "062000038F")
   expect_equal(r$logger_id[2], "GR10DATA")
+
+  expect_equal(substr(r$time, 1, 10), as.character(r$date))
 
   # No dst
   expect_equal(load_raw(f)$time[2], as.POSIXct("2016-01-11 10:48:50", tz = tz_offset(check_tz(Sys.timezone()), tz_name = TRUE)))
@@ -85,10 +90,13 @@ test_that("load_raw loads and formats data correctly - logger id from first line
   f <- system.file("extdata", "raw", "exp2", "GR10DATA_2016_01_16.TXT", package = "feedr")
   expect_is(r <- load_raw(r_file = f, details = 1, tz = "America/Vancouver"), "data.frame")
   expect_is(r2 <- load_raw(r_file = f, details = 1, tz = "America/Vancouver", logger_pattern = "[GPR]{2,3}[0-9]{1,2}"), "data.frame")
-  expect_match(names(r)[1:3], "^animal_id$|^time$|^logger_id$")
+  expect_match(names(r)[1:4], "^animal_id$|^date$|^time$|^logger_id$")
   expect_is(r$animal_id, "character")
   expect_is(r$logger_id, "character")
   expect_is(r$time, "POSIXct")
+  expect_is(r$date, "Date")
+
+  expect_equal(substr(r$time, 1, 10), as.character(r$date))
 
   expect_equal(r$animal_id[2], "062000038F")
   expect_true(all(r$logger_id == "GR10DATA"))
@@ -137,10 +145,13 @@ test_that("load_raw_all loads and formats data correctly", {
   expect_silent(load2 <- load_raw_all(r_dir = d, extra_pattern = "exp[0-9]{1,2}", extra_name = "Experiment", tz = "America/Vancouver", tz_disp = "America/Toronto", verbose = FALSE))
 
   expect_is(load, "data.frame")
-  expect_match(names(load)[1:3], "^animal_id$|^time$|^logger_id$")
+  expect_match(names(load)[1:3], "^animal_id$|^date$|^time$|^logger_id$")
   expect_is(load$animal_id, "factor")
   expect_is(load$logger_id, "factor")
   expect_is(load$time, "POSIXct")
+  expect_is(load$date, "Date")
+
+  expect_equal(substr(load$time, 1, 10), as.character(load$date))
 
   expect_equal(load$animal_id[1], factor("06200004BF", levels =  c("0000000000", "011017A536", "011017A605", "03000314F9", "0620000062", "062000014F", "06200001F0", "06200002E7", "0620000380", "062000038D", "062000038F", "0620000392", "06200003A7", "06200003B4", "06200003C3", "06200003F3", "0620000400", "0620000418", "06200004A9", "06200004BB", "06200004BE", "06200004BF", "06200004E4", "0620000525", "07008D9E08", "0700ED9E0E", "0700EDAB15", "0700EDF012", "0700EDF015", "0700EE022B", "0700EE0E42", "0700EE1461", "0700EE1467", "0700EE147F", "0700EE1504", "0700EE19CE", "0700EE2B10", "0700EE2B11")))
   expect_equal(load$logger_id[1], factor("GR10DATA", levels = c("GR10DATA", "GR11DATA", "GR12DATA", "GR13DATA")))
@@ -154,7 +165,7 @@ test_that("dl_data loads and formats data correctly", {
   expect_silent(load <- dl_data(start = "2016-01-01", end = "2016-02-01"))
   expect_silent(load2 <- dl_data(start = "2016-01-01", end = "2016-02-01", tz_disp = "America/Toronto"))
   expect_is(load, "data.frame")
-  expect_match(names(load)[1:3], "^animal_id$|^time$|^logger_id$")
+  expect_match(names(load)[1:4], "^animal_id$|^date$|^time$|^logger_id$")
   expect_is(load$animal_id, "factor")
   expect_is(load$logger_id, "factor")
   expect_is(load$time, "POSIXct")
