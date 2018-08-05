cd863edded224cc451cb4ccd6853a9d0de0fa6f7
Stefanie LaZerte <steffi@steffi.ca>
2018-08-04 20:50:24 -0500

    Experimental to Mainstream

--- inst/shiny-examples/animalnexus/server.R
+++ inst/shiny-examples/animalnexus/server.R
@@ -1,5 +1,5 @@
 cat("Starting server...\n")
-library(feedr, lib.loc = "/usr/local/lib/R_exp/site-library/")
+library(feedr, lib.loc = "/usr/local/lib/R/site-library/")
 library(magrittr)
 library(shiny)
 library(shinyjs)
