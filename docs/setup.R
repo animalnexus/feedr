c6376fb05896049aa8eb635cab9122cfff51fe40
Steffi LaZerte <steffi@steffi.ca>
2026-01-16 15:26:02 -0600

    Convert docs website to pkgdown (rough)

--- docs/setup.R
+++
@@ -1,8 +0,0 @@
-library(feedr)
-library(tidyverse)
-library(lubridate)
-
-options(dplyr.width = Inf)
-knitr::opts_chunk$set(cache = FALSE)
-knitr::opts_chunk$set(fig.width = 8, fig.height = 5)
-options(width = 10000)
